5ea33971d02238ee25e01f73206bd1871b822b56
John Helder <johnhelderjp@gmail.com>
2022-12-16 20:37:20 -0300

    Pegando as informações com a função

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,6 +9,16 @@ function showQuestion(){
   if(questions[currentQuestion]){
     let q = questions[currentQuestion];
 
+    document.querySelector('.scoreArea').style.display = 'none';
+    document.querySelector('.questionArea').style.display = 'block';
+
+    document.querySelector('.question').innerHTML = q.question;
+    
+    let optionsHtml = '';
+    for(let i in q.options){
+      optionsHtml += `<div>${q.options[i]}</div>`;
+    }
+    document.querySelector('.options').innerHTML = optionsHtml;
   }else{
     //acabaram as questões
   }
